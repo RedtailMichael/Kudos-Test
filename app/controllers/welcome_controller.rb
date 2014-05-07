26eede10e91ae311147024e4b0ace079f90eae99
RedtailMichael <michael@redtailtechnology.com>
2014-05-07 15:42:00 -0700

    layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,9 +3,14 @@ class WelcomeController < ActionController::Base
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
 
-    layout 'default'
+    layout :users_layout
 
     def index
-
+        @current_user = true
     end
+
+    private
+        def users_layout
+            @current_user ? 'admin' : 'default'
+        end
 end
