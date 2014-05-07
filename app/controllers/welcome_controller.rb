a5306285825eb8f2fa4f1adff89954757faee623
RedtailMichael <michael@redtailtechnology.com>
2014-05-07 15:10:17 -0700

    start

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,8 @@ class WelcomeController < ActionController::Base
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
 
+    layout 'default'
+
     def index
 
     end
