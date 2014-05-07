448918eabd04495c8aa3c76413e68b79c6309a88
RedtailMichael <michael@redtailtechnology.com>
2014-05-07 11:18:39 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KudosTest::Application.config.secret_key_base = '7333f6bfc3e24aa42bfdb4812cdb4992849cee419dbcd4e7224cc7b437e0a5f5327fefdb1d645f4f306ff2c1bc75c253f1979bc7eef52d60ac49b10ee5cd167f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KudosTest::Application.config.session_store :cookie_store, key: '_Kudos-Test_session'
